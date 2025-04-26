b57f4ad642559a85ed4f49c4706ed1b299ee1081
Om Jee Mishra <omjeem558@gmail.com>
2025-04-27 00:19:24 +0530

    dashboard setup

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Shiksha Tech Education - School Management System",
+  description: "Comprehensive school management solution for modern educational institutions.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body className="font-sans">
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashBoard() {
+    return <div>
+        here is my Dashboard page 
+    </div>
+}

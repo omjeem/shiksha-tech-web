d45d54c50c5585e885f44d7e2cb87bd2bca5891d
Om Jee Mishra <omjeem558@gmail.com>
2025-04-28 03:57:36 +0530

    double nav bar fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 import AppDownload from "@/components/Landing/AppDownload";
 import Contact from "@/components/Landing/Contact";
 import Features from "@/components/Landing/Features";
-import Footer from "@/components/Landing/Footer";
 import Hero from "@/components/Landing/Hero";
-import Navbar from "@/components/Landing/Navbar";
 import Pricing from "@/components/Landing/Pricing";
 import Testimonials from "@/components/Landing/Testimonials";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Hero />
       <Features />
       <Testimonials />
       <AppDownload />
       <Pricing />
       <Contact />
-      <Footer />
     </main>
   );
 }
